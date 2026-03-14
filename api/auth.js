9d8945da6c18784d35e4b4ae294298fc9f5ca69f
Shef Reynolds <shef.reynolds@gmail.com>
2026-03-13 21:55:25 -0400

    Fix auth callback URL

--- api/auth.js
+++ api/auth.js
@@ -1,74 +1,9 @@
-// OAuth proxy for Decap CMS GitHub authentication
-const https = require('https');
-
-function exchangeCodeForToken(clientId, clientSecret, code) {
-  return new Promise((resolve, reject) => {
-    const body = JSON.stringify({ client_id: clientId, client_secret: clientSecret, code });
-    const options = {
-      hostname: 'github.com',
-      path: '/login/oauth/access_token',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Content-Length': Buffer.byteLength(body),
-      },
-    };
-    const req = https.request(options, (resp) => {
-      let data = '';
-      resp.on('data', (chunk) => { data += chunk; });
-      resp.on('end', () => {
-        try { resolve(JSON.parse(data)); }
-        catch (e) { reject(new Error('Bad response: ' + data)); }
-      });
-    });
-    req.on('error', reject);
-    req.write(body);
-    req.end();
+// OAuth proxy for Decap CMS - Step 1: redirect to GitHub
+module.exports = function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.GITHUB_CLIENT_ID,
+    redirect_uri: 'https://journal.shefreynolds.com/api/callback',
+    scope: 'repo',
   });
-}
-
-module.exports = async function handler(req, res) {
-  // Parse query string explicitly from the URL
-  const fullUrl = 'https://journal.shefreynolds.com' + req.url;
-  const parsedUrl = new URL(fullUrl);
-  const code = parsedUrl.searchParams.get('code');
-
-  console.log('auth handler called, url:', req.url, 'code present:', !!code);
-
-  if (!code) {
-    const params = new URLSearchParams({
-      client_id: process.env.GITHUB_CLIENT_ID,
-      redirect_uri: 'https://journal.shefreynolds.com/api/auth',
-      scope: 'repo',
-    });
-    return res.redirect(`https://github.com/login/oauth/authorize?${params}`);
-  }
-
-  try {
-    const data = await exchangeCodeForToken(
-      process.env.GITHUB_CLIENT_ID,
-      process.env.GITHUB_CLIENT_SECRET,
-      code
-    );
-
-    console.log('token exchange result:', data.access_token ? 'success' : JSON.stringify(data));
-
-    if (data.access_token) {
-      res.setHeader('Content-Type', 'text/html');
-      res.send(`
-        <script>
-          const token = "${data.access_token}";
-          const msg = JSON.stringify({ token, provider: "github" });
-          window.opener.postMessage('authorization:github:success:' + msg, '*');
-          window.close();
-        </script>
-      `);
-    } else {
-      res.status(401).send('Authentication failed: ' + JSON.stringify(data));
-    }
-  } catch (err) {
-    console.error('auth error:', err.message);
-    res.status(500).send('Server error: ' + err.message);
-  }
+  res.redirect(`https://github.com/login/oauth/authorize?${params}`);
 };
